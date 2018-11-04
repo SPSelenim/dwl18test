cb9e25a9e20d2aa214a99f631cc879ba6c3523d1
SPSelenim <44751493+SPSelenim@users.noreply.github.com>
2018-11-04 22:32:37 +0530

    Add files via upload
    test

---
+++ dwl18artid/src/main/java/dwl18grp/dwl18artid/App.java
@@ -0,0 +1,13 @@
+package dwl18grp.dwl18artid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
