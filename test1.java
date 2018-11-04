31cb6a05dd26e2d7614110c9830114abe51d6869
SPSelenim <44751493+SPSelenim@users.noreply.github.com>
2018-11-04 22:24:34 +0530

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,10 @@
+package dwl18grp.dwl18artid;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+  @Test
+  public void f() {
+	  System.out.println("First Test NG");
+  }
+}
---
+++ test2.java
@@ -0,0 +1,11 @@
+package dwl18grp.dwl18artid;
+
+import org.testng.annotations.Test;
+
+public class test2 {
+  @Test
+  public void f() {
+  System.out.println("Second Test NG");
+  }
+  
+}
